08b7937181adaea673207a60e9fbaf89625fd189
ctroncoso <schmickcl@gmail.com>
2024-07-14 04:09:24 -0400

    set portal timeout

--- src/globals.h
+++ src/globals.h
@@ -8,6 +8,7 @@
 #define PROGRAM_VERSION    "1.0.0" 
 #define CO2_LOW (700)
 #define CO2_HIGH (800)
+#define PORTAL_TIMEOUT (180) // Timeout in seconds
 
 const unsigned long measurementDelay = 60000;
 const unsigned long updateDelay = 60000*10; // 10 minute delay between update check
