73176d030892f0d003096feeaaeeb137131bd031
ctroncoso <schmickcl@gmail.com>
2024-07-15 05:34:27 -0400

    Initial state should be initialized

--- src/globals.h
+++ src/globals.h
@@ -23,7 +23,7 @@ JsonDocument doc;
 char serializedString[200];
 
 enum class CO2_Condition { Green, Yellow, Red, Unknown };
-CO2_Condition co2_State;
+CO2_Condition co2_State=CO2_Condition::Unknown;
 
 WiFiClient espClient;
 WiFiManager wm;
