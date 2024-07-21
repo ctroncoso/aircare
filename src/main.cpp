f5295df2cebc55ea1095b994b64c4196d8bcba19
ctroncoso <schmickcl@gmail.com>
2024-07-21 16:47:05 -0400

    prettified

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,8 @@
 int alarm_level = 0;
 OneButton button(GPIO_NUM_0);
 
-void handleMultiClick() {
+void handleMultiClick()
+{
   int clicks = button.getNumberClicks();
   switch (clicks)
   {
@@ -27,46 +28,41 @@ void handleMultiClick() {
     Serial.println(clicks);
     break;
   }
-
-
 }
-
 
 void configModeCallback(WiFiManager *wm);
 void startWifiPortal();
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  while(!Serial);
+  while (!Serial)
+    ;
 
-
-  leds::initLEDS();  //in globals.h
+  leds::initLEDS(); // in globals.h
 
   pinMode(rlPin1, OUTPUT);
   pinMode(rlPin2, OUTPUT);
 
-  digitalWrite(rlPin1,HIGH);
+  digitalWrite(rlPin1, HIGH);
   digitalWrite(rlPin2, HIGH);
 
   delay(10000);
 
-  digitalWrite(rlPin1,LOW);
+  digitalWrite(rlPin1, LOW);
   digitalWrite(rlPin2, LOW);
 
   co2_State = CO2_Condition::Unknown;
   button.attachDoubleClick(startWifiPortal);
   button.attachMultiClick(handleMultiClick);
-  //button.attachMultiClick(ota::checkUpdate,)
+  // button.attachMultiClick(ota::checkUpdate,)
 
-
-  
   Serial.printf("Board: %s \n", ARDUINO_BOARD);
   Serial.printf("Version: %s \n", PROGRAM_VERSION);
   Serial.print("MAC: ");
-  Serial.println( String(WiFi.macAddress()));
+  Serial.println(String(WiFi.macAddress()));
 
   wifiM::initWifiM();
-
 
   ntp::initNTP();
   sunriseH::initCo2Sensor();
@@ -74,7 +70,8 @@ void setup() {
   mqtt::initMQTT();
 }
 
-void loop() { 
+void loop()
+{
   button.tick();
   unsigned long currentTime = millis();
 
@@ -82,12 +79,11 @@ void loop() {
 
   // check for updates and install.
   updateTick();
-
 }
 
-void startWifiPortal(){
+void startWifiPortal()
+{
   Serial.println("Portal Triggered...");
   wm.startConfigPortal(PORTAL_NAME);
   ESP.restart();
 }
-
